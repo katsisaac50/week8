65bb68500031dbc16890fe9b70bdb9e7894d1648
katsisaac50 <33658908+katsisaac50@users.noreply.github.com>
2018-02-14 07:10:52 +0100

    Add files via upload

--- main.js
+++ main.js
@@ -1,4 +1,23 @@
-console.log('script loaded');
+console.log("script loaded");
+let numbers = [1, 2, 3, 4];
+let newNumbers = [];
+
+for(let i = 0; i < numbers.length; i++) {
+    if(numbers[i] % 2 !== 0) {
+        newNumbers[i] = numbers[i] * 2;
+    }
+}
+
+console.log("The doubled numbers are", newNumbers);
+
+
+
+
+let oddNumbers = numbers.filter(x=>x%2!==0);
+doubledOddNumbers=oddNumbers.map(x=>x*2);
+console.log(doubledOddNumbers);
+
+
 
 function fetchJsonData(url, callBackFunction) {
     const request = new XMLHttpRequest();
@@ -10,79 +29,22 @@ function fetchJsonData(url, callBackFunction) {
     request.send();
 }
 
-fetchJsonData("https://api.spacexdata.com/v2/launches", function(jsonData) {
+fetchJsonData("https://gist.githubusercontent.com/pankaj28843/08f397fcea7c760a99206bcb0ae8d0a4/raw/02d8bc9ec9a73e463b13c44df77a87255def5ab9/movies.json", function(jsonData) {
     console.log(jsonData);
-    document.getElementById("demo").innerHTML = jsonData[0].rocket.rocket_name;
+    movies=jsonData;
+    //console.log(movies.filter(x=>x.rating>9));
+    var newArray = movies.map((x)=>x.rating>=7 ? 'good':'bad')
+    console.log(newArray);
+    
+    
 });
 
-const api_key = '2FuF3E9nyFeXWt3aHIfkxtKTUGn73v0w';
-const query = 'success';
-const giphyUrl = 'http://api.giphy.com/v1/gifs/search?api_key=' + api_key + '&q=' + query;
-
-
-fetchJsonData(giphyUrl, function(giphyData) {
-    console.log(giphyData.data);
-    document.getElementById("demo").innerHTML = giphyData;
-    let gif_url = [];
-    let gifData = giphyData.data;
-    for (let i = 0; i < gifData.length; i++) {
-        gif_url.push(gifData[i]);
+fetchJsonData("https://gist.githubusercontent.com/pankaj28843/08f397fcea7c760a99206bcb0ae8d0a4/raw/02d8bc9ec9a73e463b13c44df77a87255def5ab9/movies.json",function moviesRatings(movies){
+    console.log(moviesRatings());
+    var total = 0,
+        i;
+    for (i = 0; i < movies.length; i += 1) {
+        total += movies[i];
     }
-    for (let i = 0; i < gif_url.length; i++) {
-        console.log(gif_url[i]);
-        let container2 = document.createElement("div");
-        container2.className = "grid";
-        let title = document.createElement("h2");
-        let ul = document.createElement("ul");
-        let url = document.createElement("li");
-        let giphyImg = document.createElement("img");
-        document.querySelector("body").appendChild(container2);
-
-        container2.appendChild(title);
-        container2.appendChild(ul);
-        ul.appendChild(url);
-        container2.appendChild(giphyImg);
-        title.innerHTML = gif_url[i].title;
-        giphyImg.src = gif_url[i].images.preview_gif.url;
-        url.innerHTML = gif_url[i].url;
-
-    }
-
-
-
-
-});
-const astroData = "http://api.open-notify.org/astros.json"
-
-
-
-function addFields(url, callBackFunction) {
-    console.log(url);
-
-    const request1 = new XMLHttpRequest();
-    request1.addEventListener('load', function() {
-        callBackFunction(JSON.parse(request1.responseText));
-    });
-
-    request1.open('get', url);
-    request1.send();
-}
-fetchJsonData("https://api.open-notify.org/astros.json", function(jsonData) {
-    console.log(jsonData);
-    let astronunts = [];
-    let astroData = jsonData.people;
-    for (let i = 0; i < astroData.length; i++) {
-        astronunts.push(astroData[i].name);
-        console.log(astronunts);
-    }
-    var number = document.getElementById("astronut").value;
-    var container = document.getElementById("container");
-    while (container.hasChildNodes()) {
-        container.removeChild(container.lastChild);
-    }
-    for (i = 0; i < 6; i++) {
-        container.appendChild(document.createTextNode((i + 1) + ". " + astroData[i].name));
-
-        container.appendChild(document.createElement("br"));
-    }
+    return total / movies.length;
 });
